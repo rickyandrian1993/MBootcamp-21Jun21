6fe67f3090f8624e024d49f8a804c5dfff27ba79
ricky-andrian <rickyandrian1993@gmail.com>
2021-06-24 18:06:51 +0700

    remove console.log

--- 02-hooks/src/component/UserCard.js
+++ 02-hooks/src/component/UserCard.js
@@ -3,10 +3,9 @@ import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function UserCard(props) {
-  console.log(props);
   return (
     <div>
-      <Card className="mx-4 my-2 shadow" style={{ width: '18rem', color: '#000'}}>
+      <Card className="mx-4 my-2 shadow" style={{ width: '18rem', color: '#000' }}>
         <Card.Img className="rounded" variant="top" src={props.data.picture.medium} />
         <Card.Body>
           <Card.Title className="text-center">{props.data.name.title}. {props.data.name.first} {props.data.name.last}</Card.Title>
